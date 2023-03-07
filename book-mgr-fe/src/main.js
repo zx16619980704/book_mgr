86517c122325620e8f003afed0363021e56914d9
zx16619980704 <m16619980704@163.com>
2023-03-07 15:33:21 +0800

    完成书籍页面的前端界面

--- book-mgr-fe/src/main.js
+++ book-mgr-fe/src/main.js
@@ -4,5 +4,11 @@ import router from "./router";
 import store from "./store";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
+import SpaceBetween from "./components/SpaceBetween";
 
-createApp(App).use(store).use(router).use(Antd).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(Antd)
+  .component("space-between", SpaceBetween)
+  .mount("#app");
--- book-mgr-fe/src/views/Books/index.vue
+++ book-mgr-fe/src/views/Books/index.vue
@@ -1,8 +1,24 @@
 <template>
-  <div>books</div>
+  <div>
+    <a-card>
+      <h2>图书列表</h2>
+      <a-divider />
+      <space-between>
+        <div class="search">
+          <a-input-search placeholder="根据书名搜索" enter-button />
+        </div>
+        <a-button>添加一条</a-button>
+      </space-between>
+      <a-divider />
+      <a-table :columns="columns" :data-source="data">
+
+      </a-table>
+    </a-card>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+</script>
 
 <style lang="scss" scoped>
 @import './index.scss'
